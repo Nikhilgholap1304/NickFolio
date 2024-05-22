9174af54cd91f52552b204885f7e29557be609ac
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-05-22 18:51:56 +0530

    update

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Section from "./Section";
+import Reveal from "./Reveal";
+
+
+const Testimonials = () => {
+  return (
+    <>
+      <Section
+        className="flex flex-col mt-[4rem] lg:mt-[10rem] lg:px-[6rem] md:px-[6rem] sm:px-[1.25rem] xs:px-[1.25rem] 2xs:px-[1.25rem] lg:gap-[3rem] gap-[1rem] relative"
+        id="projects"
+      >
+        <div className="flex items-center justify-between gap-4 sm:gap-10">
+          <Roller direction="right" id="4" />
+          <h1 className="font-black font-Poppins text-[3rem] sm:text-[3rem] xs:text-[3rem] 2xs:text-[2rem] lg:text-6xl">
+            <Reveal>
+              Testimonials&nbsp;<span className="text-violet-500">.</span>
+            </Reveal>
+          </h1>
+        </div>
+      </Section>
+    </>
+  );
+};
+
+export default Testimonials;
