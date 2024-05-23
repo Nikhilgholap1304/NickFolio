6ff3e694eee1042b3a25be922731b2e889126012
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-05-23 13:09:34 +0530

    update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import Section from "./Section";
 import Reveal from "./Reveal";
 import Roller from "./Roller";
-
+import { motion, useTransform, useScroll } from "framer-motion";
 
 const Testimonials = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0,1],["1%","-95%"]);
+
   return (
     <>
       <Section
@@ -20,8 +27,19 @@ const Testimonials = () => {
           </h1>
         </div>
       </Section>
-      <div className="bg-violet-600 h-[300vh] relative z-1">
-        <div className="sticky top-[10%]">HII</div>
+      <div className="h-[300vh] relative z-1" ref={targetRef}>
+        <div className="sticky top-0 bg-violet-600 h-screen flex items-center overflow-hidden px-5">
+          <motion.div className="flex gap-10" style={{x}}>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+            <div className=" h-[20rem] w-[15rem] bg-yellow-400 rounded-md"></div>
+          </motion.div>
+        </div>
       </div>
     </>
   );
