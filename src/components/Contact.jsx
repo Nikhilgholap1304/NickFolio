ebe5fee90a727f9bb8757370f27bb5a9a5c5adee
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-05-26 20:42:49 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
           <Reveal>
             <p className="my-6 font-extralight text-center text-xl 2xs:text-base">
               Feel free to shoot me an email if you're not enough busy and thus
-              want to connect! You can also find me on{" "} yes
+              want to connect! You can also find me on{" "}
               <a
                 href="https://www.linkedin.com/in/nikhil-n-gholap/"
                 className="text-violet-500 hover:underline"
